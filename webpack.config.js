90435384dccbfe4a7859d181ac60396a6cc90ac3
resetus <resetus@gmail.com>
2017-11-11 13:31:02 +0300

    add jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
-
 const pug = require('./webpack/pug');
 const devServer = require('./webpack/devserver');
-
 const sass = require('./webpack/sass');
 const css = require('./webpack/css');
 const extractCSS = require('./webpack/css.extract');
+const uglifyJS = require('./webpack/js.uglify');
 
 const PATHS = {
 	sourсe: path.join(__dirname, 'source'),
@@ -30,19 +28,28 @@ const common = merge([
 		},
 
 		plugins: [
+
 			new HtmlWebpackPlugin({
 				filename: 'index.html',
 				chunks: ['index', 'common'],
 				template: PATHS.sourсe + '/pages/index/index.pug',
 			}),
+
 			new HtmlWebpackPlugin({
 				filename: 'example.html',
 				chunks: ['example', 'common'],
 				template: PATHS.sourсe + '/pages/example/example.pug',
 			}),
+
 			new webpack.optimize.CommonsChunkPlugin({
 				name: 'common',
-			})
+			}),
+
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery'
+			}),
+
 		],
 	},
 
@@ -55,7 +62,8 @@ module.exports = function(env) {
 	if (env == 'production') {
 		return merge([
 			common,
-			extractCSS()
+			extractCSS(),
+			uglifyJS(),
 		]);
 	}
 
